6781ec8d290587541f1188bc3df9aef650bd983c
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-12-08 18:29:29 +0100

    Dataflyt

--- stages/download/inn_nin-data.js
+++
@@ -1,6 +0,0 @@
-const { git } = require("lastejobb");
-
-git.clone(
-  "https://github.com/Artsdatabanken/landskap-ubehandlet.git",
-  "temp/landskap-ubehandlet"
-);
--- stages/transform/09/url.js
+++
@@ -1,8 +0,0 @@
-const { io, json, url } = require("lastejobb");
-
-let tre = json.arrayToObject(io.lesDatafil("landskap").items, {
-  uniqueKey: "kode"
-});
-
-new url(tre).assignUrls();
-io.skrivBuildfil("type", tre);
